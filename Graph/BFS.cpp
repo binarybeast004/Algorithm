164d5f69d79689234ac307f1ab09530dda3f4c8f
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-12-05 18:53:49 +0600

    Update BFS.cpp

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -11,45 +11,47 @@ using namespace std;
 #define FOR(i,n)    for(int i=0;i<n;i++)
 #define rep(i,k,n)  for(int i=k; i<n; i++)
 #define repp(i,k,n) for(int i=k; i<=n; i++)
-#define N           7   //number of node
-vector<int> adj[N];     //adj array create in a vector 
-int vis[N];             //Visited array 
 
-void BFS(int node) {    //bfs method
-    queue<int> q;       //create a queue - q 
-    q.push(node);       //first Insert the node into queue
-    vis[node] = 1;      //make the node visited
+vector<vector<int>>adj;  
+   //adj array create in a vector 
+void BFS(int node,int vis[]){
+    queue<int> q;
+    q.push(node);
+    vis[node] = 1;
 
-    while(!q.empty()) {        //Until Queue q is empty
-        int n1 = q.front();    //keep the front(queue) node in n1 variable  
-        q.pop();               //delete the node from queue q
-        cout << n1 << " ";     //print the node
-        for(int i : adj[n1]) { //Foreach loop //
-            if(!vis[i]) {      //if not visited
-                q.push(i);     //value insert into queue 
-                vis[i] = 1;    //make i index visited
+    while(!q.empty()){
+        int n1=q.front();
+        q.pop();
+        //cout<<n1<<" ";
+        for(int i: adj[n1]){
+            if(!vis[i]){
+                q.push(i);
+                vis[i]=1;
             }
         }
     }
 }
-
-int main() {
+int main(){
     BOOST;
-
+    int count=0;
+    int n;
+    cin>>n;
+    adj.assign(n+1, vector<int>());
     int m;
-    cin >> m ;
-    for(int i=0; i<m; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);     //for adjacent list issue intert the u into v . and v into u.
+    cin>>m;
+    rep(i,0,m){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
         adj[v].push_back(u);
     }
-
-    for(int i=0; i<N; i++) {     //if there is no connection with to components , thats why we check visited or not visited
-        if(!vis[i]) {            //if Not visited
-            BFS(i);              //use recursive call
+    int vis[n+1]={0};
+    rep(i,1,n+1){
+        if(!vis[i]){
+            BFS(i,vis);
+            count++;
         }
     }
-
+    cout<<count<<endl;
     return 0;
 }
